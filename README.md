6ab7c7716ca0e7f1367e13188ed055ca6d3ef20d
九林 <jokerdoc@163.com>
2019-04-09 16:27:26 +0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# 我的博客 | 技术交流 | 学习笔记
+
+[查看](https://jokerdoc.github.io/blog/)
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  base: '/blog/',
   title: '九林个人博客',
   description: '技术分享 | 学习成长',
   themeConfig: {
