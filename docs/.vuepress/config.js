b768cbd7ecc8b63a892ecf4e7ccc21ba3ec98f3a
九林 <jokerdoc@163.com>
2019-04-26 17:19:01 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,18 @@
 module.exports = {
   base: '/blog/',
+
   title: '九林个人博客',
+
   description: '技术分享 | 学习成长',
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@images': 'images'
+      }
+    }
+  },
+
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
@@ -14,20 +25,20 @@ module.exports = {
       {
         title: '环境部署',
         children: [
-          '/sys/installnodejs',
-          '/sys/installmongodb',
-          '/sys/installredis',
-          '/sys/installyapi',
-          '/sys/pm2',
+          '/article/sys/installnodejs',
+          '/article/sys/installmongodb',
+          '/article/sys/installredis',
+          '/article/sys/installyapi',
+          '/article/sys/pm2',
         ]
       },
 
       {
         title: '前端技术笔记',
         children: [
-          '/fe/gis'
+          '/article/fe/gis'
         ]
       },
     ]
-  }
+  },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,3 @@
-::: tip
-前言
-:::
+> 博观而约取，厚积而薄发
 
+![Image from alias](./images/lm.jpg)
